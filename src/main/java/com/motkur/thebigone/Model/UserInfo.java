fb4ae7a6c8305e5f88c9c7fa206778ebf4b1bfdb
Wojciech <wojciech.kur96@gmail.com>
2019-06-02 22:01:28 +0200

    User info updated

--- src/main/java/com/motkur/thebigone/Model/UserInfo.java
+++ src/main/java/com/motkur/thebigone/Model/UserInfo.java
@@ -8,12 +8,12 @@ public class UserInfo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @Column(name = "full_name")
-    private String fullName;
+    private String name;
+    private String surname;
     private String address;
     @Column(name = "post_code")
     private String postCode;
-    @OneToOne(mappedBy = "user_info")
+    @OneToOne(mappedBy = "userInfo")
     private User user;
 
     public long getId() {
@@ -24,12 +24,20 @@ public class UserInfo {
         this.id = id;
     }
 
-    public String getFullName() {
-        return fullName;
+    public String getName() {
+        return name;
     }
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
     }
 
     public String getAddress() {
